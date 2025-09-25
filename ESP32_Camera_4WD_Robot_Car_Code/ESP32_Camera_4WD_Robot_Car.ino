2a08c9facdcbf3245a07c40496cfb84bfb63484b
Rishi Mukherjee <mukhe100@umn.edu>
2025-09-25 09:21:31 -0500

    ino file

--- ESP32_Camera_4WD_Robot_Car_Code/ESP32_Camera_4WD_Robot_Car.ino
+++ ESP32_Camera_4WD_Robot_Car_Code/ESP32_Camera_4WD_Robot_Car.ino
@@ -1,46 +1,26 @@
+// ESP32_Camera_4WD_Robot_Car.ino
+// Main sketch file
 
 #include "esp_camera.h"
 #include <WiFi.h>
 
-// Select camera model
-//#define CAMERA_MODEL_WROVER_KIT
-//#define CAMERA_MODEL_M5STACK_PSRAM
+// =======================
+// WiFi Credentials - UPDATE THESE!
+// =======================
+const char* ssid = "SSID";
+const char* password = "PASSWORD";
+
+// =======================
+// Camera Model
+// =======================
 #define CAMERA_MODEL_AI_THINKER
-const char* ssid1 = "ESP32-CAM Robot";
-const char* password1 = "";
 
-extern void robot_stop();
-extern void robot_setup();
-
-
-
-#if defined(CAMERA_MODEL_WROVER_KIT)
-#define PWDN_GPIO_NUM    -1
-#define RESET_GPIO_NUM   -1
-#define XCLK_GPIO_NUM    21
-#define SIOD_GPIO_NUM    26
-#define SIOC_GPIO_NUM    27
-
-#define Y9_GPIO_NUM      35
-#define Y8_GPIO_NUM      34
-#define Y7_GPIO_NUM      39
-#define Y6_GPIO_NUM      36
-#define Y5_GPIO_NUM      19
-#define Y4_GPIO_NUM      18
-#define Y3_GPIO_NUM       5 
-#define Y2_GPIO_NUM       4
-#define VSYNC_GPIO_NUM   25
-#define HREF_GPIO_NUM    23
-#define PCLK_GPIO_NUM    22
-
-
-#elif defined(CAMERA_MODEL_AI_THINKER)
+#ifdef CAMERA_MODEL_AI_THINKER
 #define PWDN_GPIO_NUM     32
 #define RESET_GPIO_NUM    -1
 #define XCLK_GPIO_NUM      0
 #define SIOD_GPIO_NUM     26
 #define SIOC_GPIO_NUM     27
-
 #define Y9_GPIO_NUM       35
 #define Y8_GPIO_NUM       34
 #define Y7_GPIO_NUM       39
@@ -52,99 +32,112 @@ extern void robot_setup();
 #define VSYNC_GPIO_NUM    25
 #define HREF_GPIO_NUM     23
 #define PCLK_GPIO_NUM     22
-
-#else
-#error "Camera model not selected"
 #endif
-extern int gpLed =  4; // Light
-extern String WiFiAddr ="";
 
-void startCameraServer();
+// =======================
+// Global Variables
+// =======================
+int gpLed = 4;  // LED Flash pin
+String WiFiAddr = "";
 
+// =======================
+// External Function Declarations
+// =======================
+extern void startCameraServer();
+extern void robot_setup();
+extern void robot_stop();
+
+// =======================
+// Setup Function
+// =======================
 void setup() {
-  Serial.begin(115200);
-  Serial.setDebugOutput(true);
-  Serial.println();
-  robot_setup();
-  pinMode(gpLed, OUTPUT); //Light
-  digitalWrite(gpLed, LOW);
+    Serial.begin(115200);
+    Serial.setDebugOutput(true);
+    Serial.println("\n\nESP32-CAM Robot Starting...");
 
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-  //init with high specs to pre-allocate larger buffers
-  if(psramFound()){
-    config.frame_size = FRAMESIZE_UXGA;
-    config.jpeg_quality = 10;
-    config.fb_count = 2;
-  } else {
-    config.frame_size = FRAMESIZE_SVGA;
-    config.jpeg_quality = 12;
-    config.fb_count = 1;
-  }
+    // Initialize LED
+    pinMode(gpLed, OUTPUT);
+    digitalWrite(gpLed, LOW);
 
-  // camera init
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    return;
-  }
+    // Initialize motors
+    robot_setup();
 
-  //drop down frame size for higher initial frame rate
-  sensor_t * s = esp_camera_sensor_get();
-  s->set_framesize(s, FRAMESIZE_CIF);
+    // Camera configuration
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sscb_sda = SIOD_GPIO_NUM;
+    config.pin_sscb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    config.pixel_format = PIXFORMAT_JPEG;
 
-  // WiFi.begin(ssid, password);
+    // Frame buffer configuration
+    if(psramFound()) {
+        config.frame_size = FRAMESIZE_VGA;
+        config.jpeg_quality = 10;
+        config.fb_count = 2;
+    } else {
+        config.frame_size = FRAMESIZE_CIF;
+        config.jpeg_quality = 12;
+        config.fb_count = 1;
+    }
 
-  // while (WiFi.status() != WL_CONNECTED) {
-  //   delay(500);
-  //   Serial.print(".");
-  // }
-  // Serial.println("");
-  // Serial.println("WiFi connected");
+    // Initialize camera
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK) {
+        Serial.printf("Camera init failed with error 0x%x\n", err);
+        return;
+    }
 
-  // startCameraServer();
+    // Drop down frame size for higher initial frame rate
+    sensor_t * s = esp_camera_sensor_get();
+    s->set_framesize(s, FRAMESIZE_CIF);
 
-  // Serial.print("Camera Ready! Use 'http://");
-  // Serial.print(WiFi.localIP());
-  // WiFiAddr = WiFi.localIP().toString();
-  // Serial.println("' to connect");
-  
-  WiFi.softAP(ssid1, password1);
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(myIP);
+    // Connect to WiFi
+    WiFi.begin(ssid, password);
+    Serial.println("Connecting to WiFi...");
+    
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    
+    Serial.println("\n====================================");
+    Serial.println("WiFi Connected!");
+    Serial.print("Camera Ready! Use 'http://");
+    Serial.print(WiFi.localIP());
+    WiFiAddr = WiFi.localIP().toString();
+    Serial.println("' to connect");
+    Serial.print("Stream URL: http://");
+    Serial.print(WiFiAddr);
+    Serial.println(":81/stream");
+    Serial.println("====================================\n");
 
-  Serial.print("Camera Ready! Use 'http://");
-  Serial.print(WiFi.softAPIP());
-  WiFiAddr = WiFi.softAPIP().toString();
-  Serial.println("' to connect");
-  startCameraServer();
-  digitalWrite(33,LOW);
-
+    // Start camera server
+    startCameraServer();
+    
+    // Initialize pin 33 (might be motor enable)
+    pinMode(33, OUTPUT);
+    digitalWrite(33, LOW);
 }
 
-void loop() 
-{
-  
-// put your main code here, to run repeatedly:
-
+// =======================
+// Loop Function
+// =======================
+void loop() {
+    delay(10000);
 }
